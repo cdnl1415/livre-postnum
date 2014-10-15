2f7c3ec60d635c4d2ec3e4177baccb270ef554ce
kivnek69 <kivnek69@gmail.com>
2014-10-15 13:52:16 +0200

    BoukellaRafik.phtml

--- public/index.php
+++ public/index.php
@@ -10,12 +10,9 @@ defined('APPLICATION_ENV')
 
 // Ensure library/ is on include_path
 set_include_path(implode(PATH_SEPARATOR, array(
-    realpath(APPLICATION_PATH . '/../../zend/library'),
-    realpath(APPLICATION_PATH . '/../../library'),
+    realpath(APPLICATION_PATH . '/../../Zend/library'),
     get_include_path(),
 )));
-
-
 
 /** Zend_Application */
 require_once 'Zend/Application.php';
