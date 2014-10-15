ea3d8ed65cc9958f8e8fbeefe2043e5cfd730712
ZinebS <z.slaoui91@gmail.com>
2014-10-15 12:52:40 +0200

    envoi projet zend

---
+++ trombinoscope/ZinebSlaoui/quickstart/application/controllers/TrombinoscopeController.php
@@ -0,0 +1,22 @@
+<?php
+
+class TrombinoscopeController extends Zend_Controller_Action
+{
+
+    public function init()
+    {
+        /* Initialize action controller here */
+    }
+
+    public function indexAction()
+    {
+        // action body
+    }
+
+	public function trombinoscopeAction()
+    {
+        // action body
+    }
+
+}
+
