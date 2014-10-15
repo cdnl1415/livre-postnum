94f07a2beb504640ab05c64f15e40698dfa0c6d7
hmessar <ali.hmessar@gmail.com>
2014-10-15 13:26:40 +0200

    envoi du projet

---
+++ trombinoscope/HMESSARAbdelali/application/controllers/QuickstartController.php
@@ -0,0 +1,21 @@
+<?php
+
+class QuickstartController extends Zend_Controller_Action
+{
+
+    public function init()
+    {
+        /* Initialize action controller here */
+    }
+
+    public function indexAction()
+    {
+        // action body
+    }
+     public AfficherAction()
+	 {
+		Echo 'Function Afficher';
+	 }
+
+}
+
