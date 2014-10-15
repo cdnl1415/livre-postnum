26d22b43e0567e5c3bb16c6744e565f996e0a1d5
Samuel Szoniecky <samszon@gmail.com>
2014-10-15 14:02:17 +0200

    nouveau conflit

--- application/controllers/TrombinoscopeController.php
+++ application/controllers/TrombinoscopeController.php
@@ -28,11 +28,7 @@ class TrombinoscopeController extends Zend_Controller_Action
     public function mehdiamaskaneAction(){
     
     }
-<<<<<<< HEAD
 	public function zinebslaouiAction(){
-=======
-	public function zamelmouhanedAction(){
->>>>>>> origin/master
     
     }
     
