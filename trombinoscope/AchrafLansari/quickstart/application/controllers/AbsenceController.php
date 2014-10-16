edf1436f61b6fb05a4989d14763a03f00ae3f757
achraf <achraflansari@gmail.com>
2014-10-16 13:03:24 +0200

    envoie projet zend
    quickstart

---
+++ trombinoscope/AchrafLansari/quickstart/application/controllers/AbsenceController.php
@@ -0,0 +1,27 @@
+<?php
+
+class AbsenceController extends Zend_Controller_Action
+{
+
+    public function init()
+    {
+        /* Initialize action controller here */
+    }
+
+    public function indexAction()
+    {
+        // action body
+    }
+    
+    public function absenceAction()
+    {
+          
+    }
+    
+    public function cours_offAction()
+    {
+          
+    }
+
+}
+
