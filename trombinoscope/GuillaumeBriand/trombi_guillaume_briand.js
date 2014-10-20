74942555fca2015f451c9df8c394abd3e26d6ce3
C2P0 <gpro.briand@gmail.com>
2014-10-20 11:08:56 +0200

    Classement - Guillaume Briand

---
+++ trombinoscope/GuillaumeBriand/trombi_guillaume_briand.js
@@ -0,0 +1,44 @@
+		var ficSVG = "../../svg/trombi_guillaume_briand.svg";
+		
+		function load(){
+		 	//charge l'IHM SVG
+		 	chargeIHM();
+		}
+
+		//charge le svg
+	    //merci à http://bl.ocks.org/KoGor/8162640
+	    function chargeIHM(){
+	    	queue()
+	    		.defer(d3.xml, ficSVG, "image/svg+xml")
+	    		.await(IHMcharge);
+	    }
+	    function IHMcharge(error, xml){
+	    	//Adding our svg file to HTML document
+			var importedNode = document.importNode(xml.documentElement, true);
+    	  	d3.select("#viz").node().appendChild(importedNode);	    	    	  	
+	    }		
+	    
+		// fonction pour ajouter un écouteur à UN ÉLÉMENT
+		//merci à https://developer.mozilla.org/fr/docs/DOM/element.addEventListener
+		function ajoutelesevenementauxelements() { 
+		     var el = document.getElementById("rect12259"); 
+		     el.addEventListener("click", afficheDetailEtu, false); 
+		   } 		
+		
+		/*
+		 merci à http://www.commentcamarche.net/forum/affich-21135416-html-afficher-cacher-une-div-sur-clique-a
+		 */
+		function switchInfoPerso()
+		{
+			divInfo = document.getElementById('divacacher');
+			if (divInfo.style.display == 'none')
+				divInfo.style.display = 'block';
+			else
+				divInfo.style.display = 'none';
+		}
+		
+		function afficheDetailEtu(){
+			alert("OK");
+			//var elemEtu = document.getElementById('g16893');
+			
+		}
