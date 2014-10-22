52db18edfe66157f83425edafbc9ce783e4e019b
msknmehdi <msknmehdi@gmail.com>
2014-10-22 14:52:17 +0200

    mise à jour rss

--- trombinoscope/AmaskaneMehdi/thyp1415_AmaskaneMehdi.php
+++ trombinoscope/AmaskaneMehdi/thyp1415_AmaskaneMehdi.php
@@ -3,7 +3,8 @@
 <script type="text/javascript" src="./trombinoscope.js" ></script>
 <?php
 //charge le flux rss dans un objet PHP
-$xml = simplexml_load_file('./trombinoscope.rss');
+//$xml = simplexml_load_file('./trombinoscope.rss');
+$xml = simplexml_load_file('https://picasaweb.google.com/data/feed/base/user/107401320610499259896/albumid/6065229773950541889?alt=rss&kind=photo&authkey=Gv1sRgCNak7e60l-7VlgE&hl=fr');
 $HtmlString = "<h1 class='title'>Trombinoscope Master 2 Technologies de l'Hypermédia 2014/2015</h1><div class='content'>";
 $test= "test";
 foreach ($xml->channel->item as $tof) {
