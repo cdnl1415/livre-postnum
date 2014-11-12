9b9e2b3ebb4bd54c45f42ddc2c4be454c9b86d85
zlned <badmohaned@gmail.com>
2014-11-12 16:24:57 +0100

    n

--- public/index.php
+++ public/index.php
@@ -3,13 +3,13 @@
 defined('APPLICATION_PATH')
     || define('APPLICATION_PATH', realpath(dirname(__FILE__) . '/../application'));
 
-// Define application environment
+// Define application ssenvironment
 defined('APPLICATION_ENV')
     || define('APPLICATION_ENV', (getenv('APPLICATION_ENV') ? getenv('APPLICATION_ENV') : 'production'));
 
 // Ensure library/ is on include_path
 set_include_path(implode(PATH_SEPARATOR, array(
-    realpath(APPLICATION_PATH . '/../../Zend/library'),
+    realpath(APPLICATION_PATH . '/../../zend/library'),
     get_include_path(),
 )));
 
